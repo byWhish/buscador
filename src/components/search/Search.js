cbce43e355dbe52aba4b542bb64a0a669587ae87
byWhish <gerardolareu@hotmail.com>
2020-01-18 19:08:54 -0300

    style(search): styling component

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import styles from './Search.module.css';
+import history from '../../utils/History';
 
 const Search = () => {
+    const [query, setQuery] = useState('');
+
+    const handleSearchClick = useCallback(() => {
+        history.push(`/items?search=${query}`)
+    }, [query]);
+
+    const handleSearchChange = useCallback((event) => {
+        setQuery(event.target.value);
+    }, [])
+
     return (
         <div className={styles.searchWrapper}>
             <img className={styles.searchLogo} src='/img/Logo_ML.png' alt='ML_Logo' />
-            <input className={styles.queryInput} />
-            <input type='submit' />
+            <div className={styles.inputWrapper}>
+                <input className={styles.queryInput} onChange={handleSearchChange} value={query} />
+                <button onClick={handleSearchClick}>
+                    <img src='/img/ic_Search.png' alt='buttonIcon' />
+                </button>
+            </div>
         </div>
     );
 }
---
+++ src/services/ItemsService.js
@@ -0,0 +1,15 @@
+const searchItems = (query) => {
+}
+
+const fetchItem = (id) => {
+}
+
+export const ItemService = {
+    searchItems,
+    fetchItem
+}
+
+export default {
+    searchItems,
+     fetchItem
+};
