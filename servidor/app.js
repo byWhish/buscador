ed0b34605fc7222275416632e352bd7a7e599c72
Gerardo Caceres <gpcaceres@teco.com.ar>
2020-04-03 16:23:39 -0300

    chore(): add swagger

--- servidor/app.js
+++ servidor/app.js
@@ -5,12 +5,16 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var users = require('./routes/items');
 
+var swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json');
+
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1/items', users);
 
 module.exports = app;
--- servidor/routes/items.js
+++ servidor/routes/items.js
@@ -4,14 +4,16 @@ var { searchItems, getItem } = require('../services/items');
 
 router.get('/', function(req, res, next) {
   const { params, query, headers: { author } } = req;
-  const parsedAuthor = JSON.parse(author);
+  // const parsedAuthor = JSON.parse(author);
+  const parsedAuthor = '';
   searchItems(query)
     .then((response) => res.json({ ...response, author: parsedAuthor}));
 });
 
 router.get('/:id', function(req, res, next) {
   const { params: { id }, query, headers: { author } } = req;
-  const parsedAuthor = JSON.parse(author);
+  // const parsedAuthor = JSON.parse(author);
+  const parsedAuthor = '';
   getItem(id)
     .then((response) => res.json({ ...response, author: parsedAuthor }))
 });
